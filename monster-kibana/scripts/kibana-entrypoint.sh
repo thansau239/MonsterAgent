e0bf84f55b3d59f8a95f504f1e217ac8eec29471
taehong <rlaxoghd91@gmail.com>
2018-06-20 00:59:38 +0900

    Modified ELK Config

--- monster-kibana/scripts/kibana-entrypoint.sh
+++ monster-kibana/scripts/kibana-entrypoint.sh
@@ -4,16 +4,16 @@
 # https://github.com/Cyb3rWard0g/HELK/blob/master/helk-kibana
 
 # *********** Start Kibana services ***************
-echo "[MONSTER-DOCKER-INSTALLATION-INFO] Waiting for elasticsearch URI to be accessible.."
+echo "[INFO] Waiting for elasticsearch URI to be accessible.."
 until curl -s mstr-elasticsearch:9200 -o /dev/null; do
     sleep 1
 done
 
-echo "[MONSTER-DOCKER-INSTALLATION-INFO] Starting Kibana service.."
+echo "[INFO] Starting Kibana service.."
 exec /usr/local/bin/kibana-docker &
 
 # *********** Creating Kibana Dashboards, visualizations and index-patterns ***************
-echo "[MONSTER-DOCKER-INSTALLATION-INFO] Running helk_kibana_setup.sh script..."
+echo "[INFO] Running helk_kibana_setup.sh script..."
 ./kibana-setup.sh
 
 tail -f /dev/null
